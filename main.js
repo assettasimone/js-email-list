f164b9ef2d58615ec19634e82572764eca48eed8
assettasimone <assettasimone@gmail.com>
2025-08-04 18:04:35 +0200

    add fetch api call

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 /* 
-Descrizione
-
 Attraverso l'apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
@@ -13,3 +11,18 @@ Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le a
 Buon divertimento e confermate lettura come al solito  */
 
 
+//conservo l'indirizzo API in una variabile
+
+const APIUrl = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+//test Chiamata API con Fetch
+
+fetch(APIUrl)
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data);
+
+    })
+    .catch(error => {
+        console.error(error);
+    })
